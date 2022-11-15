18487a14534069ad16188196ac3ebfbbd89f4e0f
Stephen Obia <68517142+stephenobia22@users.noreply.github.com>
2022-11-14 23:26:24 -0600

    Update TodoUpdate.ts

--- backend/src/models/TodoUpdate.ts
+++ backend/src/models/TodoUpdate.ts
@@ -1,5 +1,9 @@
-export interface TodoUpdate {
+export interface TodoItem {
+  userId: string
+  todoId: string
+  createdAt: string
   name: string
   dueDate: string
   done: boolean
+  attachmentUrl?: string
 }
