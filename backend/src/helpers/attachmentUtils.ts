89a0a4505380afc06f8fe58cf305669f26c4619d
Stephen Obia <68517142+stephenobia22@users.noreply.github.com>
2022-11-14 23:22:29 -0600

    Update attachmentUtils.ts

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -1,33 +1,20 @@
-import 'source-map-support/register'
-
 import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
-import { integer } from 'aws-sdk/clients/cloudfront'
 
 const XAWS = AWSXRay.captureAWS(AWS)
 
+const bucketName = process.env.ATTACHMENT_S3_BUCKET;
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION;
+
+const s3 = new XAWS.S3({
+    signatureVersion: "v4",
+  });
+
 // TODO: Implement the fileStogare logic
-
-export class AttachmentUtils {
-  constructor(
-    private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
-    private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
-    private readonly urlExpiration: integer = parseInt(
-      process.env.SIGNED_URL_EXPIRATION
-    )
-  ) {}
-
-  async getAttachmentUrl(attachmentId: string): Promise<string> {
-    const attachmentUrl = `https://${this.bucketName}.s3.amazonaws.com/${attachmentId}`
-    return attachmentUrl
+export async function getUploadUrl(todoId: string): Promise<string> {
+    return s3.getSignedUrl("putObject", {
+      Bucket: bucketName,
+      Key: todoId,
+      Expires: parseInt(urlExpiration),
+    });
   }
-
-  async getUploadUrl(attachmentId: string): Promise<string> {
-    const uploadUrl = this.s3.getSignedUrl('putObject', {
-      Bucket: this.bucketName,
-      Key: attachmentId,
-      Expires: this.urlExpiration
-    })
-    return uploadUrl
-  }
-}
